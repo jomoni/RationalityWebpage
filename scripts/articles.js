e9969a9b9341d3097427a0a6bcf4232dc49e8d3a
Morgan Nicholson <Morgan.Nicholson@intrackradar.com>
2020-06-09 17:41:54 -0600

    initial articles layout wuth carousel

---
+++ scripts/articles.js
@@ -0,0 +1,31 @@
+var slideInterval = 3500;
+var pointer = 0;
+
+function getFigures() {
+    var figures = document.getElementById("carousel").querySelectorAll("figure");
+    return figures;
+}
+
+function moveForward() {
+    var figures = getFigures();
+    for (var i = 0; i < figures.length; i++) {
+        if (figures[i].className == 'visible') {
+            figures[i].className = '';
+        }
+    }
+    if (pointer + 1 == figures.length) {
+        pointer = 0;
+    } else {
+        pointer = pointer + 1;
+    }
+    figures[pointer].className = 'visible';
+    setTimeout(moveForward, slideInterval);
+}
+
+function startPlayback(){
+    var figures = getFigures();
+    figures[0].className = 'visible'
+    setTimeout(moveForward, slideInterval);
+}
+
+startPlayback()
